a0821a124efc3b0b016f7b68835df8d97882c0c1
ElylaHamada <130731509+ElylaHamada@users.noreply.github.com>
2023-04-13 23:04:11 -0700

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -10,42 +10,63 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // reads the number of PA and make an array of the scores based on the amt of PA
+        // this should read the number of PA and their scores
         int numAssignments = scanner.nextInt();
-        int[] assignmentScores = new int[numAssignments];
+        double[] assignmentScores = new double[numAssignments];
         double totalScore = 0.0;
         for (int i = 0; i < numAssignments; i++) {
-            assignmentScores[i] = scanner.nextInt();
-            totalScore += assignmentScores[i];
+            if (scanner.hasNextDouble()) {
+                double score = scanner.nextDouble();
+                if (score < 0) {
+                    System.out.println("invalid input");
+                    return;
+                }
+                assignmentScores[i] = score;
+                totalScore += score;
+            } else {
+                System.out.println("invalid input");
+                return;
+            }
         }
         double averageScore = totalScore / numAssignments;
 
-        // read the scores for the midterm and final exams
-        int midtermScore = scanner.nextInt();
-        int finalScore = scanner.nextInt();
+        // this one reades the scores of the midterm and final
+        if (scanner.hasNextDouble()) {
+            double midtermScore = scanner.nextDouble();
+            double finalScore = scanner.nextDouble();
 
-        // Calculate total score
-        double paWeight = 0.5;
-        double midtermWeight = 0.125;
-        double finalWeight = 0.375;
-        totalScore = averageScore * paWeight + midtermScore * midtermWeight + finalScore * finalWeight;
+            // checks for any negative values
+            if (midtermScore < 0 || finalScore < 0) {
+                System.out.println("invalid input");
+                return;
+            }
 
-        // Determine letter grade
-        String letterGrade;
-        if (totalScore >= 90) {
-            letterGrade = "A";
-        } else if (totalScore >= 80) {
-            letterGrade = "B";
-        } else if (totalScore >= 70) {
-            letterGrade = "C";
-        } else if (totalScore >= 60) {
-            letterGrade = "D";
+            // Calculate total score
+            double paWeight = 0.5;
+            double midtermWeight = 0.125;
+            double finalWeight = 0.375;
+            totalScore = averageScore * paWeight + midtermScore * midtermWeight + finalScore * finalWeight;
+
+            // Determine letter grade
+            String letterGrade;
+            if (totalScore >= 90) {
+                letterGrade = "A";
+            } else if (totalScore >= 80) {
+                letterGrade = "B";
+            } else if (totalScore >= 70) {
+                letterGrade = "C";
+            } else if (totalScore >= 60) {
+                letterGrade = "D";
+            } else {
+                letterGrade = "F";
+            }
+
+            // Display results
+            System.out.println("Programming assignment average: " + averageScore);
+            System.out.println("Total score: " + totalScore);
+            System.out.println("Letter grade: " + letterGrade);
         } else {
-            letterGrade = "F";
+            System.out.println("Invalid input");
         }
-
-        // Display results
-        System.out.println("Total score: " + totalScore);
-        System.out.println("Letter grade: " + letterGrade);
     }
 }
